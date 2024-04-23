b2040b4daa0071ddf9dea898c547f78896c43094
venkatesh7975 <venkateshmoyya7975@gmail.com>
2024-04-23 15:04:56 +0530

    super over league cover page

---
+++ index.js
@@ -0,0 +1,10 @@
+const element = (
+    <div>
+      <h1 className="greeting">Super Over League</h1>
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" alt="rcb" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" alt="csk" />
+    </div>
+  );
+  
+  ReactDOM.render(element, document.getElementById("root"));
+  
